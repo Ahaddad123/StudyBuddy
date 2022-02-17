1dd64bff297fbd5a5dae9ad2260d380f228b7758
Ender717 <nathan.sandvig@gmail.com>
2022-02-17 16:41:41 -0600

    Created the project for Homework 1 - Nathan S, 2-17-22

---
+++ HW1/SE3330HW1/SE3330HW1Tests/SE3330HW1Tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SE3330HW1Tests
+{
+	TEST_CLASS(SE3330HW1Tests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
