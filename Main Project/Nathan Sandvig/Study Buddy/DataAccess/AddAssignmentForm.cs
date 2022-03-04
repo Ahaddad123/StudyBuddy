b4c481415006cc4b9fb1ed3c0cf4628b6de52904
AndrewJVonderhaar <100882364+AndrewJVonderhaar@users.noreply.github.com>
2022-03-03 22:11:21 -0600

    Andrew Vonderhaar- Added AddAssignmentForm and AddCourseForm

---
+++ Main Project/Nathan Sandvig/Study Buddy/DataAccess/AddAssignmentForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Study_Buddy.DataAccess
+{
+    public partial class AddAssignmentForm : Form
+    {
+        public AddAssignmentForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
