2ceb9916aba457dc5ef2cf94dfb0bf4993e1ceae
AndrewJVonderhaar <100882364+AndrewJVonderhaar@users.noreply.github.com>
2022-03-03 22:59:14 -0600

    Andrew Vonderhaar - added features to AddAssignment form and AddCourseForm

--- Main Project/Nathan Sandvig/Study Buddy/DataAccess/AddAssignmentForm.cs
+++ Main Project/Nathan Sandvig/Study Buddy/DataAccess/AddAssignmentForm.cs
@@ -16,5 +16,10 @@ namespace Study_Buddy.DataAccess
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
