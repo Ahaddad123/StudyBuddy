1a4b671ec7895d681562fc81e8e48c36577ce0d0
AndrewJVonderhaar <100882364+AndrewJVonderhaar@users.noreply.github.com>
2022-03-06 09:29:21 -0600

    Andrew Vonderhaar - psudo code for study log

--- Main Project/Nathan Sandvig/Study Buddy/DataAccess/StudyLog.cs
+++ Main Project/Nathan Sandvig/Study Buddy/DataAccess/StudyLog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,18 @@ namespace Study_Buddy.DataAccess
 {
     internal class StudyLog
     {
+        //wip find out how to impliment map
+
+        public void AddTimeToLog(double newTime)
+        {
+
+        }
+
+        /*
+         * public double GetHours(int day)
+         * {
+         *  return log<day, double>;
+         *  }
+         */
     }
 }
