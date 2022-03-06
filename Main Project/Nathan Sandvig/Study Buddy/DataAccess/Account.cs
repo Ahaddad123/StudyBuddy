0c8d68774722b2d3f8a1961a4b806ed14abd5f4f
Andrew <haddada@uwplatt.edu>
2022-03-06 17:25:49 -0600

    Committing Initial Account stuff

--- Main Project/Nathan Sandvig/Study Buddy/DataAccess/Account.cs
+++ Main Project/Nathan Sandvig/Study Buddy/DataAccess/Account.cs
@@ -8,5 +8,32 @@ namespace Study_Buddy.DataAccess
 {
     internal class Account
     {
+        internal String username; //Login username
+        internal String password; //Login password
+        internal int userID; //This is the ID for the database to track accounts
+
+        public Account(String newUsername, String newPassword, int newUserID){
+            username = newUsername;
+            password = newPassword;
+            userID = newUserID;
+        }
+
+        public Boolean CreateAccount(String username, String password) {
+            Boolean AccountCreated = false;
+
+            return AccountCreated;
+        }
+
+        public Boolean Login(String username, String password)
+        {
+            Boolean isCorrect = false;
+
+            return isCorrect;
+        }
+
+
+        public void addCourse() { }
+
+        public void removeCourse() { }
     }
 }
