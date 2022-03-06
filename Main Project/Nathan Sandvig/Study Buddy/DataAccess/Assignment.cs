a310d684c76a62cdf9cd2f4d799466dd9beb5e65
AndrewJVonderhaar <100882364+AndrewJVonderhaar@users.noreply.github.com>
2022-03-05 21:52:19 -0600

    Andrew Vonderhaar - Added commets to variables

--- Main Project/Nathan Sandvig/Study Buddy/DataAccess/Assignment.cs
+++ Main Project/Nathan Sandvig/Study Buddy/DataAccess/Assignment.cs
@@ -8,15 +8,21 @@ namespace Study_Buddy.DataAccess
 {
     internal class Assignment
     {
-        internal int points;
-        internal string name;
-        internal int priority;
+        internal int points;         //total point value of assignment
+        internal string name;        //name of assignment
+        internal int priority;       //priority points of assignments
+        internal double gradePoints; //gade user got for assignment
 
         public Assignment(int newPoints, string newName, int newPriority)
         {
             points = newPoints;
             name = newName;
             priority = newPriority;
+        }
+
+        void gradeAssignment(double percentGrade)
+        {
+            gradePoints = points * (0.01 * percentGrade);
         }
         
     }
--- Main Project/Nathan Sandvig/Study Buddy/DataAccess/Course.cs
+++ Main Project/Nathan Sandvig/Study Buddy/DataAccess/Course.cs
@@ -8,13 +8,13 @@ namespace Study_Buddy.DataAccess
 {
     internal class Course
     {
-        List<Assignment> assignments;
-        String courseName;
-        double courseGrade;
-        String courseCode;
-        int coursePriority;
-        int courseCredits;
-        int courseDifficulty;
+        List<Assignment> assignments; //list of assignments for course
+        String courseName;            //name of course
+        double courseGrade;           //grade for course
+        String courseCode;            //code for course
+        int coursePriority;           //priority of course
+        int courseCredits;            //credit value of course
+        int courseDifficulty;         //difficulty value of course
         public Course(string newName, double newGrade, String newCode, int newPriority, int newCredit, int newDiff)
         {
             courseName = newName;
@@ -29,7 +29,7 @@ namespace Study_Buddy.DataAccess
         public void AddAssigment(string assignName, int assignPoint, int assignPriority)
         {
             Assignment temp = new Assignment(assignPoint, assignName, assignPriority);
-
+            //wip add assignments based on priority
             assignments.Add(temp);
         }
 
