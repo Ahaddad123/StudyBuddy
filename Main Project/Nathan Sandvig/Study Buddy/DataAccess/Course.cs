da540f418ae2e082345cad352dcc9fd191b49d00
Ender717 <nathan.sandvig@gmail.com>
2022-03-06 19:05:13 -0600

    #29 Added course builder file
    Having an internal class for building courses didn't work, so I made the class external

--- Main Project/Nathan Sandvig/Study Buddy/DataAccess/Course.cs
+++ Main Project/Nathan Sandvig/Study Buddy/DataAccess/Course.cs
@@ -32,110 +32,23 @@ namespace Study_Buddy.DataAccess
         private double credits;
 
         //---------------------------------------------------------------------
-        // This class is a builder class for Courses
-        // v1: Created the class - Nathan S, 3-6-22
-        //---------------------------------------------------------------------
-        public class CourseBuilder
-        {
-            //-----------------------------------------------------------------
-            // Private data members
-            // name: The name of the course
-            // grade: The grade the user currently has in the course
-            // code: The code of the course
-            // priority: How important the course is
-            // credits: The number of credits the course is worth
-            //-----------------------------------------------------------------
-            public String name;
-            public double grade;
-            public String code;
-            public double priority;
-            public double credits;
-
-            //-----------------------------------------------------------------
-            // Default constructor for the CourseBuilder class
-            // v1: Created the constructor - Nathan S, 3-6-22
-            //-----------------------------------------------------------------
-            public CourseBuilder()
-            {
-
-            }
-
-            //-----------------------------------------------------------------
-            // Wither method for the name attribute
-            // name: The name being set in the builder
-            // return: The builder for build chains
-            // v1: Created the method - Nathan S, 3-6-22
-            //-----------------------------------------------------------------
-            public CourseBuilder WithName(String name)
-            {
-                this.name = name;
-                return this;
-            }
-
-            //-----------------------------------------------------------------
-            // Wither method for the grade attribute
-            // grade: The grade being set in the builder
-            // return: The builder for build chains
-            // v1: Created the method - Nathan S, 3-6-22
-            //-----------------------------------------------------------------
-            public CourseBuilder WithGrade(double grade)
-            {
-                this.grade = grade;
-                return this;
-            }
-
-            //-----------------------------------------------------------------
-            // Wither method for the code attribute
-            // code: The code being set in the builder
-            // return: The builder for build chains
-            // v1: Created the method - Nathan S, 3-6-22
-            //-----------------------------------------------------------------
-            public CourseBuilder WithCode(String code)
-            {
-                this.code = code;
-                return this;
-            }
-
-            //-----------------------------------------------------------------
-            // Wither method for the priority attribute
-            // priority: The priority being set in the builder
-            // return: The builder for build chains
-            // v1: Created the method - Nathan S, 3-6-22
-            //-----------------------------------------------------------------
-            public CourseBuilder WithPriority(double priority)
-            {
-                this.priority = priority;
-                return this;
-            }
-
-            //-----------------------------------------------------------------
-            // Wither method for the credits attribute
-            // credits: The credits being set in the builder
-            // return: The builder for build chains
-            // v1: Created the method - Nathan S, 3-6-22
-            //-----------------------------------------------------------------
-            public CourseBuilder WithCredits(double credits)
-            {
-                this.credits = credits;
-                return this;
-            }
-
-            //-----------------------------------------------------------------
-            // Build method for the builder
-            // return: The course which was built
-            // v1: Created the method - Nathan S, 3-6-22
-            //-----------------------------------------------------------------
-            public Course Build()
-            {
-                return new Course(this);
-            }
-        }
-
-        //---------------------------------------------------------------------
         // Default constructor for the Course class
         // v1: Created the method - Nathan S, 3-6-22
         //---------------------------------------------------------------------
-        private Course(CourseBuilder builder)
+        public Course()
+        {
+            this.name = "CourseName";
+            this.grade = 100.0;
+            this.code = "CourseCode";
+            this.priority = -1.0;
+            this.credits = 3.0;
+        }
+
+        //---------------------------------------------------------------------
+        // Builder constructor for the Course class
+        // v1: Created the method - Nathan S, 3-6-22
+        //---------------------------------------------------------------------
+        public Course(CourseBuilder builder)
         {
             this.name = builder.name;
             this.grade = builder.grade;
---
+++ Main Project/Nathan Sandvig/Study Buddy/DataAccess/CourseBuilder.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Study_Buddy.DataAccess;
+
+namespace Study_Buddy.DataAccess
+{
+    //---------------------------------------------------------------------
+    // This class is a builder class for Courses
+    // v1: Created the class - Nathan S, 3-6-22
+    //---------------------------------------------------------------------
+    internal class CourseBuilder
+    {
+        //-----------------------------------------------------------------
+        // Private data members
+        // name: The name of the course
+        // grade: The grade the user currently has in the course
+        // code: The code of the course
+        // priority: How important the course is
+        // credits: The number of credits the course is worth
+        //-----------------------------------------------------------------
+        public String name;
+        public double grade;
+        public String code;
+        public double priority;
+        public double credits;
+
+        //-----------------------------------------------------------------
+        // Default constructor for the CourseBuilder class
+        // v1: Created the constructor - Nathan S, 3-6-22
+        //-----------------------------------------------------------------
+        public CourseBuilder()
+        {
+
+        }
+
+        //-----------------------------------------------------------------
+        // Wither method for the name attribute
+        // name: The name being set in the builder
+        // return: The builder for build chains
+        // v1: Created the method - Nathan S, 3-6-22
+        //-----------------------------------------------------------------
+        public CourseBuilder WithName(String name)
+        {
+            this.name = name;
+            return this;
+        }
+
+        //-----------------------------------------------------------------
+        // Wither method for the grade attribute
+        // grade: The grade being set in the builder
+        // return: The builder for build chains
+        // v1: Created the method - Nathan S, 3-6-22
+        //-----------------------------------------------------------------
+        public CourseBuilder WithGrade(double grade)
+        {
+            this.grade = grade;
+            return this;
+        }
+
+        //-----------------------------------------------------------------
+        // Wither method for the code attribute
+        // code: The code being set in the builder
+        // return: The builder for build chains
+        // v1: Created the method - Nathan S, 3-6-22
+        //-----------------------------------------------------------------
+        public CourseBuilder WithCode(String code)
+        {
+            this.code = code;
+            return this;
+        }
+
+        //-----------------------------------------------------------------
+        // Wither method for the priority attribute
+        // priority: The priority being set in the builder
+        // return: The builder for build chains
+        // v1: Created the method - Nathan S, 3-6-22
+        //-----------------------------------------------------------------
+        public CourseBuilder WithPriority(double priority)
+        {
+            this.priority = priority;
+            return this;
+        }
+
+        //-----------------------------------------------------------------
+        // Wither method for the credits attribute
+        // credits: The credits being set in the builder
+        // return: The builder for build chains
+        // v1: Created the method - Nathan S, 3-6-22
+        //-----------------------------------------------------------------
+        public CourseBuilder WithCredits(double credits)
+        {
+            this.credits = credits;
+            return this;
+        }
+
+        //-----------------------------------------------------------------
+        // Build method for the builder
+        // return: The course which was built
+        // v1: Created the method - Nathan S, 3-6-22
+        //-----------------------------------------------------------------
+        public Course Build()
+        {
+            return new Course(this);
+        }
+    }
+}
