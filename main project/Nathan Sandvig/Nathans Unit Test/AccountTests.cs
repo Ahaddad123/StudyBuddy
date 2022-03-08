066f1ec85161bdc837e8847d6bd1e8969a41b1c0
Andrew <haddada@uwplatt.edu>
2022-03-08 17:18:18 -0600

    Made changes to account tests.cs

--- main project/Nathan Sandvig/Nathans Unit Test/AccountTests.cs
+++ main project/Nathan Sandvig/Nathans Unit Test/AccountTests.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using Study_Buddy.DataAccess;
+using System.Collections.Generic;
+
 
 namespace Nathans_Unit_Test
 {
@@ -22,16 +24,49 @@ namespace Nathans_Unit_Test
             Assert.AreEqual(false, account.Login("nathan123", "nathanpassword"));
         }
 
-        public void CheckAddCourse() {
-            Account account = new Account("josh123", "joshpassword", 1);
-            account.addCourse();
-            Assert.AreEqual(false, account.addCourse());
+        public void AddCourseTest()
+        {
+            Account account = new Account("username", "password", 1);
+            Course course = new Course();
+            List<Course> testList = new List<Course>();
+
+            account.addCourse(course);
+            testList.Add(course);
+
+            Assert.AreEqual(1, account.courses.Count);
+            Assert.AreEqual(testList[0], account.courses[0]);
         }
 
-        public void CheckRemoveCourse() {
-            Account account = new Account("josh123", "joshpassword", 1);
-            account.removeCourse("math");
-            Assert.AreEqual(false, account.removeCourse("math"));
+        [TestMethod]
+        public void RemoveCourseTest_NotExists()
+        {
+            Account account = new Account("username", "password", 1);
+            CourseBuilder courseBuilder = new CourseBuilder();
+            Course course1 = courseBuilder.WithName("test1").Build();
+            Course course2 = courseBuilder.WithName("test2").Build();
+
+            account.addCourse(course1);
+            account.addCourse(course2);
+
+            Assert.IsFalse(account.removeCourse("test3"));
+            Assert.AreEqual(2, account.courses.Count);
+        }
+        public void RemoveCourseTest_Exists()
+        {
+            Account account = new Account("username", "password", 1);
+            CourseBuilder courseBuilder = new CourseBuilder();
+            Course course1 = courseBuilder.WithName("test1").Build();
+            Course course2 = courseBuilder.WithName("test2").Build();
+            List<Course> testList = new List<Course>();
+
+            account.addCourse(course1);
+            account.addCourse(course2);
+            testList.Add(course1);
+
+            Assert.IsTrue(account.removeCourse("test2"));
+            Assert.AreEqual(1, account.courses.Count);
+            Assert.AreEqual(testList[0], account.courses[0]);
         }
     }
+
 }
