14d523f49a3771684d8300b993000ac2c1c94055
AndrewJVonderhaar <100882364+AndrewJVonderhaar@users.noreply.github.com>
2022-03-07 20:41:27 -0600

    Andrew Vonderhaar - Added code for study log and created unit test for studylog

---
+++ Main Project/Nathan Sandvig/Nathans Unit Test/StudyLogTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Study_Buddy.DataAccess;
+
+namespace Nathans_Unit_Test
+{
+    [TestClass]
+    public class StudyLogTests
+    {
+        [TestMethod]
+        public void AddHours_Zero()
+        {
+            StudyLog testLog = new StudyLog(1,0);
+            testLog.AddTimeToLog(2.5, 1);
+            Assert.AreEqual(2.5, testLog.GetHours(1));
+        }
+
+        [TestMethod]
+        public void AddHours_GreaterThenZero()
+        {
+            StudyLog testLog = new StudyLog(1,0);
+            testLog.AddTimeToLog(0.3, 1);
+            testLog.AddTimeToLog(0.3, 1);
+            Assert.AreEqual(0.6, testLog.GetHours(1));
+        }
+
+        [TestMethod]
+        public void AddHourse_MultipleDays()
+        {
+            StudyLog testLog = new StudyLog(1, 0);
+            StudyLog testLog2 = new StudyLog(2, 0);
+            testLog.AddTimeToLog(0.3, 2);
+            testLog2.AddTimeToLog(0.3, 3);
+            Assert.AreEqual(0.3, testLog.GetHours(2));
+        }
+    }
+}
--- Main Project/Nathan Sandvig/Study Buddy/DataAccess/StudyLog.cs
+++ Main Project/Nathan Sandvig/Study Buddy/DataAccess/StudyLog.cs
@@ -7,20 +7,56 @@ using System.Threading.Tasks;
 
 namespace Study_Buddy.DataAccess
 {
-    internal class StudyLog
+    public class StudyLog
     {
         //wip find out how to impliment map
+        Dictionary<int , double> log = new Dictionary<int , double>();
 
-        public void AddTimeToLog(double newTime)
+        //---------------------------------------------------------------------
+        // default constructor for studylog
+        // v1: Created the method - Andrew V, 3-7-22
+        //---------------------------------------------------------------------
+        public StudyLog()
         {
-
+            log = new Dictionary<int , double>();
         }
 
-        /*
-         * public double GetHours(int day)
-         * {
-         *  return log<day, double>;
-         *  }
-         */
+        //---------------------------------------------------------------------
+        // constructor for studylog
+        // v1: Created the method - Andrew V, 3-7-22
+        //---------------------------------------------------------------------
+        public StudyLog(int day, double hours)
+        {
+            log = new Dictionary<int, double>()
+            {
+                {day, hours},
+            };
+        }
+
+        //---------------------------------------------------------------------
+        // Adds study time to a specified day
+        // day:     date that the hours will be added to
+        // newTime: time that will be added to the date
+        // v1: Created the method - Andrew V, 3-7-22
+        //---------------------------------------------------------------------
+        public void AddTimeToLog(double newTime, int day)
+        {
+            if(log[day] != 0)
+            {
+                newTime = newTime + log[day];
+            }
+            log.Add(day, newTime);
+        }
+
+        //---------------------------------------------------------------------
+        // returns study hours for that date
+        // day: date of hours being returned
+        // v1: Created the method - Andrew V, 3-7-22
+        //---------------------------------------------------------------------
+        public double GetHours(int day)
+         {
+            return log[day];
+         }
+         
     }
 }
