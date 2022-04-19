6d290714119798e41aece3a4c1ab45c43cf74417
Ender717 <73455333+Ender717@users.noreply.github.com>
2022-04-19 00:18:50 -0500

    Added base parameters to the algorithm class - Nathan S, 4-19-22

--- main project/Nathan Sandvig/Study Buddy/BusinessLogic/Algorithm.cs
+++ main project/Nathan Sandvig/Study Buddy/BusinessLogic/Algorithm.cs
@@ -13,6 +13,14 @@ namespace Algorithms
     internal class Algorithm
     {
         //---------------------------------------------------------------------
+        // Constants:
+        // DEFAULT_BASE: The default base grade of the user
+        // DEFAULT_HOURS: The default hours per percentage of the user
+        //---------------------------------------------------------------------
+        private const double DEFAULT_BASE = 60;
+        private const double DEFAULT_HOURS = 0.1;
+
+        //---------------------------------------------------------------------
         // Attributes:
         // assignmentLog: The list of assignment grades and hours studied
         // baseGrade: The grade the user would get with 0 hours of studying
@@ -24,16 +32,18 @@ namespace Algorithms
 
         //---------------------------------------------------------------------
         // Default constructor for the algorithm1 class
-        // v1: Created the constructor - Nathan S, 3-24-22
+        // v1: Created the constructor - Nathan S, 4-19-22
         //---------------------------------------------------------------------
-        public Algorithm(LinkedList<Tuple<double, double>> assignmentLog)
+        public Algorithm()
         {
-            this.assignmentLog = assignmentLog;
+            assignmentLog = new LinkedList<Tuple<double, double>>();
+            baseGrade = DEFAULT_BASE;
+            hoursPerPercent = DEFAULT_HOURS;
         }
 
         //---------------------------------------------------------------------
         // Calculates the grade function
-        // v1: Created the method - Nathan S, 3-24-22
+        // v1: Created the method - Nathan S, 4-19-22
         //---------------------------------------------------------------------
         private void CalculateFunction()
         {
@@ -72,11 +82,12 @@ namespace Algorithms
         // Calculates the number of hours needed to get a target grade
         // targetGrade: The grade the user wishes to achieve
         // return: The number of hours needed for the grade
-        // v1: Created the method - Nathan S, 3-24-22
+        // v1: Created the method - Nathan S, 4-19-22
         //---------------------------------------------------------------------
         public double HoursForGrade(double targetGrade)
         {
-            CalculateFunction();
+            if (assignmentLog.Count > 0)
+                CalculateFunction();
             return (targetGrade - baseGrade) / hoursPerPercent;
         }
     }
