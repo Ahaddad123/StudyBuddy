1ab49581ba0d72bf6cbb030ee633aafa4ab7b6d9
Makenna Halvensleben <halvenslebem@uwplatt.edu>
2022-05-06 15:14:59 -0500

    Issue #94: added unit tests for Account

--- src/Nathan Sandvig/Nathans Unit Test/AccountTests.cs
+++ src/Nathan Sandvig/Nathans Unit Test/AccountTests.cs
@@ -77,20 +77,92 @@ namespace Nathans_Unit_Test
             Assert.AreEqual(testList[0], account.courses[0]);
         }
 
-        /*[TestMethod]
+        [TestMethod]
         public void testPopulateCourseList()
         {
+            int numCourses = 5;
             Account account = new Account("username", "password");
             account.database = new MockDatabase();
             List<String> courseList = new List<String>();
-            for(int i = 0; i < 5; i++)
+            for(int i = 0; i < numCourses; i++)
             {
                 courseList.Add("courseName" + i);
                 courseList.Add(3.ToString());
                 courseList.Add(i.ToString());
             }
 
-        }*/
+            account.populateCourseList(courseList);
+
+            Assert.AreEqual(numCourses, account.courses.Count);
+        }
+
+        [TestMethod]
+        public void testPopulateStudyHours()
+        {
+            int numHours1 = 1;
+            int numHours2 = 2;
+            Account account = new Account("username", "password");
+            account.database = new MockDatabase();
+            CourseBuilder courseBuilder = new CourseBuilder();
+            Course course1 = courseBuilder.WithName("test1").Build();
+            Course course2 = courseBuilder.WithName("test2").Build();
+            account.courses.Add(course1);
+            account.courses.Add(course2);
+            List<String> studyHoursList = new List<String>();
+            studyHoursList.Add(numHours1.ToString());
+            studyHoursList.Add(course1.name);
+            studyHoursList.Add(DateTime.Today.ToString());
+            studyHoursList.Add(numHours2.ToString());
+            studyHoursList.Add(course2.name);
+            studyHoursList.Add(DateTime.Today.ToString());
+
+            account.populateStudyHours(studyHoursList);
+
+            Assert.AreEqual(numHours1, account.getCourseByName(course1.name).GetHoursStudied(DateTime.Today));
+            Assert.AreEqual(numHours2, account.getCourseByName(course2.name).GetHoursStudied(DateTime.Today));
+        }
+
+        [TestMethod]
+        public void testPopulateSchedules()
+        {
+            int startTime1 = 8;
+            int endTime1 = 9;
+            int startTime2 = 1;
+            int endTime2 = 4;
+            Account account = new Account("username", "password");
+            account.database = new MockDatabase();
+            CourseBuilder courseBuilder = new CourseBuilder();
+            Course course1 = courseBuilder.WithName("test1").Build();
+            Course course2 = courseBuilder.WithName("test2").Build();
+            account.courses.Add(course1);
+            account.courses.Add(course2);
+            List<String> schedulesList = new List<String>();
+            schedulesList.Add(course1.name);
+            for(int i = 0; i < 7; i++)
+            {
+                schedulesList.Add(new DateTime(1,1,1,startTime1,0,0).ToString());
+                schedulesList.Add(new DateTime(1, 1, 1, endTime1, 0, 0).ToString());
+            }
+            schedulesList.Add(course2.name);
+            for (int i = 0; i < 7; i++)
+            {
+                schedulesList.Add(new DateTime(1, 1, 1, startTime2, 0, 0).ToString());
+                schedulesList.Add(new DateTime(1, 1, 1, endTime2, 0, 0).ToString());
+            }
+
+            account.populateSchedules(schedulesList);
+            
+            foreach(DayOfWeek day in Enum.GetValues(typeof(DayOfWeek)))
+            {
+                Assert.AreEqual(endTime1 - startTime1, account.getCourseByName(course1.name).schedule.getCourseHours(day));
+                Assert.AreEqual(endTime2 - startTime2, account.getCourseByName(course2.name).schedule.getCourseHours(day));
+                Assert.AreEqual(startTime1, account.getCourseByName(course1.name).schedule.times[day].startTime.Hour);
+                Assert.AreEqual(endTime1, account.getCourseByName(course1.name).schedule.times[day].endTime.Hour);
+                Assert.AreEqual(startTime2, account.getCourseByName(course2.name).schedule.times[day].startTime.Hour);
+                Assert.AreEqual(endTime2, account.getCourseByName(course2.name).schedule.times[day].endTime.Hour);
+            }
+            
+        }
     }
 
 }
